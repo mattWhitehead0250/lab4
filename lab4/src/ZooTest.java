56a495a8b69ba2967436baeadfc184594f7fe606
Matt Whitehead <matthew.s.whitehead-1@ou.edu>
2019-02-11 19:01:52 -0600

    Finished all tests with passing results

--- lab4/src/ZooTest.java
+++ lab4/src/ZooTest.java
@@ -15,38 +15,65 @@ public class ZooTest
 		Integer capacityActual = zoo1.getCapacity();
 		Assert.assertEquals(capacityExpected,capacityActual);
 	}
-	//test expandZoo and addAnimal
+	//test expandZoo, addAnimal, toString
 	public void expandZooTest() throws AssertException
 	{
-		Animal a = new Animal("Green", "Chester", 350.0, 40);
-	    Animal b = new Animal("Orange", "Tony", 200.5, 15);
+		Animal a = new Animal("Green", "Snake", 350.0, 40);
+	    Animal b = new Animal("Orange", "Tiger", 200.5, 15);
 	    
 		Zoo zoo1 = new Zoo(1);
 		
 		zoo1.addAnimal(a);
 		zoo1.addAnimal(b);
-		Integer capacityExpected = 4;
+		Integer capacityExpected = 2;
 		Integer capacityActual = zoo1.getCapacity();
 		Assert.assertEquals(capacityExpected, capacityActual);
+		String toStringExpected = "These animals live in the zoo: \n" + 
+				"Snake, a Green-colored animal. 350.0 pounds, 40.0 inches\n" + 
+				"Tiger, a Orange-colored animal. 200.5 pounds, 15.0 inches\n";
+		String toStringActual = zoo1.toString();
+		Assert.assertEquals(toStringExpected, toStringActual);
 	}
 	//test getTotalHeight
 	public void getTotalHeightTest() throws AssertException
 	{
-
+		Animal a = new Animal("Green", "Snake", 350.0, 40);
+		Animal b = new Animal("Orange", "Tiger", 200.5, 15);
+		
+		Zoo zoo1 = new Zoo(3);
+		zoo1.addAnimal(a);
+		zoo1.addAnimal(b);
+		
+		Double expectedHeight = 40.0 + 15.0;
+		Double actualHeight = zoo1.getTotalHeight();
+		Assert.assertEquals(expectedHeight, actualHeight, .01);
 	}
 	//test getAverageWeight
 	public void getAverageWeightTest() throws AssertException
 	{
-
+		Animal a = new Animal("Green", "Snake", 350.0, 40);
+		Animal b = new Animal("Orange", "Tiger", 200.5, 15);
+		
+		Zoo zoo1 = new Zoo(3);
+		zoo1.addAnimal(a);
+		zoo1.addAnimal(b);
+		
+		int expectedWeight = (int) ((350.0 + 200.5)/2);
+		int actualWeight = zoo1.getAverageWeight();
+		Assert.assertEquals(expectedWeight, actualWeight);
 	}
 	//test getAverageWeight by color
 	public void getAverageWeightColorTest() throws AssertException
 	{
-
-	}
-	//test toString
-	public void toStringTest() throws AssertException
-	{
-
+		Animal a = new Animal("Green", "Snake", 350.0, 40);
+		Animal b = new Animal("Orange", "Tiger", 200.5, 15);
+		
+		Zoo zoo1 = new Zoo(3);
+		zoo1.addAnimal(a);
+		zoo1.addAnimal(b);
+		
+		int expectedWeight = (int) 350.0;
+		int actualWeight = zoo1.getAverageWeight("Green");
+		Assert.assertEquals(expectedWeight, actualWeight);
 	}
 }
