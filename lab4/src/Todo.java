aa861ed359b41c2c53028c9aa0c721c10b3fed76
Matt Whitehead <matthew.s.whitehead-1@ou.edu>
2019-02-11 19:06:48 -0600

    finished project

--- lab4/src/Todo.java
+++ lab4/src/Todo.java
@@ -1,6 +1,6 @@
 public class Todo {/*
 Lab Section_________: 11
-Last Modified_______: 2019-02-XX
+Last Modified_______: 2019-02-11
 OU ID_______________: 112737541
 OU EMAIL____________: matthew.s.whitehead-1@ou.edu
 
