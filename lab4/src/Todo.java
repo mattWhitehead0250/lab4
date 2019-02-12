dad98648393d56ab5ae7f56a4f22e958294554ae
Matt Whitehead <matthew.s.whitehead-1@ou.edu>
2019-02-11 18:46:30 -0600

    added zooTest and developed test for expand zoo

--- lab4/src/Todo.java
+++ lab4/src/Todo.java
@@ -5,14 +5,53 @@ OU ID_______________: 112737541
 OU EMAIL____________: matthew.s.whitehead-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Finish animal equals method
+2. Develop test for full constructor of animal object
+3. Develop test for animal equals method
+4. Finish Equipment equals method
+5. Develop test for equipment constructor
+6. Develop test for equipment toString
+7. Develop test for equipment equals method
+8. Develop test for zoo constructor
+9. Develop test for expandZoo
+10. Develop test for addAnimal
+11. Develop test for getTotalHeight
+12. Develop test for getAverageWeight
+13. Develop test for getAverageWeight by color
+14. Develop test for getAnimals[]
+15. Develop test for zoo toString()
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 10
+2. 10
+3. 10
+4. 10
+5. 12
+6. 10
+7. 15
+8. 20
+9. 10
+10. 5
+11. 5
+12. 5
+13. 5
+14. 5
+15. 5
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 15
+2. 12
+3. 13
+4. 15
+5. 24
+6. 20
+7. 15
+8. 9
+9. 10
+10. 12
+11. 10
+12. 4
+13. 4
+14. 4
+15. 2
 */}
--- lab4/src/ZooTest.java
+++ lab4/src/ZooTest.java
@@ -10,14 +10,43 @@ public class ZooTest
 	//test zooConstructor
 	public void zooTest() throws AssertException
 	{
-	Zoo zoo1 = new Zoo(3);
-	
+		Zoo zoo1 = new Zoo(3);
+		Integer capacityExpected = 3;
+		Integer capacityActual = zoo1.getCapacity();
+		Assert.assertEquals(capacityExpected,capacityActual);
 	}
-	//test expandZoo
-	//test addAnimal
+	//test expandZoo and addAnimal
+	public void expandZooTest() throws AssertException
+	{
+		Animal a = new Animal("Green", "Chester", 350.0, 40);
+	    Animal b = new Animal("Orange", "Tony", 200.5, 15);
+	    
+		Zoo zoo1 = new Zoo(1);
+		
+		zoo1.addAnimal(a);
+		zoo1.addAnimal(b);
+		Integer capacityExpected = 4;
+		Integer capacityActual = zoo1.getCapacity();
+		Assert.assertEquals(capacityExpected, capacityActual);
+	}
 	//test getTotalHeight
+	public void getTotalHeightTest() throws AssertException
+	{
+
+	}
 	//test getAverageWeight
+	public void getAverageWeightTest() throws AssertException
+	{
+
+	}
 	//test getAverageWeight by color
-	//test getAnimals[]
-	//test toString()
+	public void getAverageWeightColorTest() throws AssertException
+	{
+
+	}
+	//test toString
+	public void toStringTest() throws AssertException
+	{
+
+	}
 }
